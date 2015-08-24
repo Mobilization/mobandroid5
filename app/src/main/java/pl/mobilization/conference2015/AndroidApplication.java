fa97f5bfe2d895eb335b103fc8dc69250c785463
Mariusz Saramak <m.saramak@samsung.com>
2015-08-24 15:40:49 +0200

    [DEBUG] add stric mode setup in debug

--- app/src/main/java/pl/mobilization/conference2015/AndroidApplication.java
+++ app/src/main/java/pl/mobilization/conference2015/AndroidApplication.java
@@ -1,6 +1,8 @@
 package pl.mobilization.conference2015;
 
 import android.app.Application;
+import android.os.StrictMode;
+
 import com.crashlytics.android.Crashlytics;
 
 /**
@@ -18,6 +20,14 @@ public class AndroidApplication extends Application {
         super.onCreate();
         Crashlytics.start(this);
         initializeInjector();
+        configureStrictMode();
+    }
+
+    private void configureStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().penaltyDeathOnNetwork().build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().detectActivityLeaks().penaltyLog().build());
+        }
     }
 
     private void initializeInjector() {
